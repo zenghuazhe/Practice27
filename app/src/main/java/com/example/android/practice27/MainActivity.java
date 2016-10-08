70515f5e6fa48d530617b7035531e7f333015898
zenghuazhe <570421628@qq.com>
2016-10-08 20:17:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/practice27/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.practice27;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.buttonSend);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.intent.action.MY_BROADCAST");
+                intent.putExtra("broadcastmsg", "hello receiver.");
+                sendBroadcast(intent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/android/practice27/MyReceiver.java
@@ -0,0 +1,19 @@
+package com.example.android.practice27;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String strMsg=intent.getStringExtra("broadcastmsg");
+        Toast.makeText(context,strMsg,Toast.LENGTH_LONG).show();
+    }
+
+}
+
